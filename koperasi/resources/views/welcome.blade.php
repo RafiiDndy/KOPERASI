9fa4d3a2c108ac3a634bf72a8ca8022c3b84ef39
Pdniell <pandudaniel17@gmail.com>
2024-04-06 14:51:12 +0700

    auth

--- koperasi/resources/views/welcome.blade.php
+++ koperasi/resources/views/welcome.blade.php
@@ -15,11 +15,12 @@
             <div>
                 @if (Route::has('login'))
                     <div class="space-x-4">
-                        @auth
-                            <a href="{{ route('login') }}" class="text-white bg-blue p-3 rounded shadow hover:bg-blue-500">login</a>
-
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-white">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="inline-block px-4 py-2 text-white rounded bg-black hover:bg-blue-500 hover:text-gray-200">Login</a>
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="text-white bg-blue p-3 rounded shadow hover:bg-red-500">Register</a>
+                                <a href="{{ route('register') }}" class="inline-block px-4 py-2 text-white bg-black p-3 rounded shadow hover:bg-red-500">Register</a>
                             @endif
                         @endauth
                     </div>
@@ -36,7 +37,7 @@
             <h1 class="text-6xl font-bold mb-8">
                 <span id="typed-text" class="inline-block"></span><span class="typing-animation"></span>
             </h1>
-            <p class="text-lg">Sebuah aplikasi yang dapat membantu membantu manajemen dan operasi koperasi serta memfasilitasi berbagai aspek kegiatan koperasi, seperti manajemen anggota, pengelolaan simpanan, pembiayaan, pelacakan transaksi, pelaporan keuangan, dan lain sebagainya. </p>
+            <p class="text-lg">Sebuah aplikasi yang dapat membantu manajemen dan operasi koperasi serta memfasilitasi berbagai aspek kegiatan koperasi, seperti manajemen anggota, pengelolaan simpanan, pelacakan transaksi, dan lain sebagainya. </p>
         </div>
         <h2 class="text-4xl text-center font-bold mb-8">Features</h2>
         <div class="flex justify-center space-x-6">
@@ -69,7 +70,7 @@
 
     <div id="simpanan-section" class="section bg-gray-200 flex items-center justify-center py-16">
         <div class="max-w-4xl mx-auto text-center text-black">
-            <h2 class="text-4xl font-bold mb-8">Jenis-jenis Simpanan</h2>
+            <h2 class="text-4xl font-bold mb-8">Jenis-Jenis Simpanan</h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
                 <div class="bg-white p-6 rounded-lg shadow hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50 transition-colors duration-200">
                     <h3 class="text-2xl font-bold mb-4">Simpanan Pokok</h3>
