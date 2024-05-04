d645ca2a70e7ec5fa22cd6a91f9b39597d3040c6
tasyarmdn <ramdani.tasya@gmail.com>
2024-05-04 18:19:27 +0700

    2-Tasya

---
+++ koperasi/resources/views/simpanan/indexNotVerified.blade.php
@@ -0,0 +1,124 @@
+<x-app-layout>
+    <div>
+        <div class="container mx-auto p-5 text-center">
+            <div class="bg-white rounded-xl shadow-2xl p-8 max-w-md mx-auto">
+                <h1 class="text-4xl font-bold mb-4 text-indigo-800 animate-pulse">Waiting for Verification</h1>
+                <p class="text-gray-600 mb-8">Please wait while we verify your information.</p>
+                <div class="flex justify-center">
+                    <div class="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-indigo-500"></div>
+                </div>
+            </div>
+        </div>
+        <div class="scene">
+            <div class="forest">
+                <div class="tree tree1">
+                    <div class="branch branch-top"></div>
+                    <div class="branch branch-middle"></div>
+                </div>
+
+                <div class="tree tree2">
+                    <div class="branch branch-top"></div>
+                    <div class="branch branch-middle"></div>
+                    <div class="branch branch-bottom"></div>
+                </div>
+
+                <div class="tree tree3">
+                    <div class="branch branch-top"></div>
+                    <div class="branch branch-middle"></div>
+                    <div class="branch branch-bottom"></div>
+                </div>
+
+                <div class="tree tree4">
+                    <div class="branch branch-top"></div>
+                    <div class="branch branch-middle"></div>
+                    <div class="branch branch-bottom"></div>
+                </div>
+
+                <div class="tree tree5">
+                    <div class="branch branch-top"></div>
+                    <div class="branch branch-middle"></div>
+                    <div class="branch branch-bottom"></div>
+                </div>
+
+                <div class="tree tree6">
+                    <div class="branch branch-top"></div>
+                    <div class="branch branch-middle"></div>
+                    <div class="branch branch-bottom"></div>
+                </div>
+
+                <div class="tree tree7">
+                    <div class="branch branch-top"></div>
+                    <div class="branch branch-middle"></div>
+                    <div class="branch branch-bottom"></div>
+                </div>
+            </div>
+
+            <div class="tent">
+                <div class="roof"></div>
+                <div class="roof-border-left">
+                    <div class="roof-border roof-border1"></div>
+                    <div class="roof-border roof-border2"></div>
+                    <div class="roof-border roof-border3"></div>
+                </div>
+                <div class="entrance">
+                    <div class="door left-door">
+                        <div class="left-door-inner"></div>
+                    </div>
+                    <div class="door right-door">
+                        <div class="right-door-inner"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="floor">
+                <div class="ground ground1"></div>
+                <div class="ground ground2"></div>
+            </div>
+
+            <div class="fireplace">
+                <div class="support"></div>
+                <div class="support"></div>
+                <div class="bar"></div>
+                <div class="hanger"></div>
+                <div class="smoke"></div>
+                <div class="pan"></div>
+                <div class="fire">
+                    <div class="line line1">
+                        <div class="particle particle1"></div>
+                        <div class="particle particle2"></div>
+                        <div class="particle particle3"></div>
+                        <div class="particle particle4"></div>
+                    </div>
+                    <div class="line line2">
+                        <div class="particle particle1"></div>
+                        <div class="particle particle2"></div>
+                        <div class="particle particle3"></div>
+                        <div class="particle particle4"></div>
+                    </div>
+                    <div class="line line3">
+                        <div class="particle particle1"></div>
+                        <div class="particle particle2"></div>
+                        <div class="particle particle3"></div>
+                        <div class="particle particle4"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="time-wrapper">
+                <div class="time">
+                    <div class="day"></div>
+                    <div class="night">
+                        <div class="moon"></div>
+                        <div class="star star1 star-big"></div>
+                        <div class="star star2 star-big"></div>
+                        <div class="star star3 star-big"></div>
+                        <div class="star star4"></div>
+                        <div class="star star5"></div>
+                        <div class="star star6"></div>
+                        <div class="star star7"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
