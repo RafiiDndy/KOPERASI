44ab0b14b1d71924d596eda4f1361aeda9115285
Pdniell <pandudaniel17@gmail.com>
2024-05-04 19:02:06 +0700

    KOPERASI - 10

--- koperasi/resources/views/recapitulation/index.blade.php
+++ koperasi/resources/views/recapitulation/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="container mx-auto px-4 py-8">
+    <div class="container mx-auto px-4 ">
         <div class="max-w-3xl mx-auto">
             <div class="text-center mb-12 text-4xl font-bold">
                 <div class="text-gray-500">Recapitulation</div>
@@ -9,6 +9,15 @@
         @livewire('recapitulation.show')
     </div>
     <div class="mt-8">
-        @livewire('recapitulation.report')
+        @livewire('recapitulation.report-pokok')
+    </div>
+    <div class="mt-8">
+        @livewire('recapitulation.report-wajib')
+    </div>
+    <div class="mt-8">
+        @livewire('recapitulation.report-sukarela')
+    </div>
+    <div class="mt-8">
+        @livewire('recapitulation.unpaid-list')
     </div>
 </x-app-layout>
