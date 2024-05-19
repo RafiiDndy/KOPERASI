6fe2c75ec7e0e92225c3f7e6e447e39303b380cb
Pdniell <pandudaniel17@gmail.com>
2024-05-19 19:05:02 +0700

    KOPERASI - 17

--- koperasi/resources/views/livewire/anggota/shu.blade.php
+++ koperasi/resources/views/livewire/anggota/shu.blade.php
@@ -1,30 +1,28 @@
-<div class="flex justify-center">
-    <div class="w-full md:max-w-2xl p-5 flex justify-between flex-row">
-        <div class="bg-gradient-to-r from-green-400 to-blue-500 rounded-lg p-6 flex items-center justify-between text-white shadow-lg w-1/2 mr-4">
-            <div>
-                <div class="text-2xl font-bold">
-                    Total SHU
-                </div>
-                <div class="text-lg">
-                    Rp.{{ number_format($shu) }}
-                </div>
+<div class="w-full max-w-2xl p-5 flex justify-between flex-row">
+    <div class="card bg-gradient-to-r from-green-500 to-blue-800 rounded-lg p-6 flex items-center justify-between text-white shadow-lg w-1/2 mr-12">
+        <div>
+            <div class="text-2xl font-bold">
+                Total SHU
             </div>
-            <div>
-                <i class="fas fa-shopping-cart text-3xl"></i>
+            <div class="text-lg">
+                Rp.{{ number_format($shu) }}
             </div>
         </div>
-        <div class="bg-gradient-to-r from-green-400 to-blue-500 rounded-lg p-6 flex items-center justify-between text-white shadow-lg w-1/2 mr-4">
-            <div>
-                <div class="text-2xl font-bold">
-                    Total Simpanan
-                </div>
-                <div class="text-lg">
-                    Rp.{{ number_format($totalSimpanan) }}
-                </div>
+        <div>
+            <i class="fas fa-shopping-cart text-3xl"></i>
+        </div>
+    </div>
+    <div class="card bg-gradient-to-r from-green-500 to-blue-800 rounded-lg p-6 flex items-center justify-between text-white shadow-lg w-1/2 mr-5">
+        <div>
+            <div class="text-2xl font-bold">
+                Total Simpanan
             </div>
-            <div>
-                <i class="fas fa-wallet text-3xl"></i>
+            <div class="text-lg">
+                Rp.{{ number_format($totalSimpanan) }}
             </div>
+        </div>
+        <div>
+            <i class="fas fa-wallet text-3xl"></i>
         </div>
     </div>
 </div>
